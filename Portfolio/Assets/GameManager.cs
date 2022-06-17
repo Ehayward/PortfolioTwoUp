857bb1904be7994373c31937245ef0c59fa27c8b
Ehayward <104617060+Ehayward@users.noreply.github.com>
2022-06-17 16:58:00 +1000

    Added ( GameManager, Coins, Tube and Physics Mats)

--- Portfolio/Assets/GameManager.cs
+++ Portfolio/Assets/GameManager.cs
@@ -1,59 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Coin Parents")]
+    [Header("Coin 1 Info")]
     //Coin 1
     public GameObject coin1Parent;
-    static Rigidbody coin1rigidbody;
+    public Rigidbody coin1Rigidbody;
     public static Vector3 coin1Velocity;
 
+    [Header("Coin 2 Info")]
     //Coin 2
     public GameObject coin2Parent;
-    static Rigidbody coin2rigidbody;
+    public Rigidbody coin2Rigidbody;
     public static Vector3 coin2Velocity;
 
+    [Header("Coin 3 Info")]
     //Coin 3
     public GameObject coin3Parent;
-    static Rigidbody coin3rigidbody;
+    public Rigidbody coin3Rigidbody;
     public static Vector3 coin3Velocity;
+
+    [Header("Buttons")]
+    public GameObject flipButton;
+
 
     void Start()
     {
-        coin1rigidbody = GetComponent<Rigidbody>();
-        coin2rigidbody = GetComponent<Rigidbody>();
-        coin3rigidbody = GetComponent<Rigidbody>();
 
-        CoinFlipper();
+
     }
 
-    void CoinFlipper()
+    public void CoinFlipper()
     {
         if ((coin1Velocity[0] == 0) && (coin1Velocity[1] == 0) && (coin1Velocity[2] == 0) && (coin2Velocity[0] == 0) && (coin2Velocity[1] == 0) && (coin2Velocity[2] == 0) && (coin3Velocity[0] == 0) && (coin3Velocity[1] == 0) && (coin3Velocity[2] == 0))
         {
-            float dirX = Random.Range(0, 500);
-            float dirY = Random.Range(0, 500);
-            float dirZ = Random.Range(110, 300);
+            float dirX1 = Random.Range(0, 500);
+            float dirY1 = Random.Range(0, 500);
+            float dirZ1 = Random.Range(110, 300);
 
-            transform.rotation = Quaternion.identity;
+            float dirX2 = Random.Range(0, 500);
+            float dirY2 = Random.Range(0, 500);
+            float dirZ2 = Random.Range(110, 300);
 
-            coin1rigidbody.AddForce(transform.up * 700);
-            coin2rigidbody.AddForce(transform.up * 1000);
-            coin3rigidbody.AddForce(transform.up * 750);
+            float dirX3 = Random.Range(0, 500);
+            float dirY3 = Random.Range(0, 500);
+            float dirZ3 = Random.Range(110, 300);
 
-            coin1rigidbody.AddTorque(dirX, dirY, dirZ);
-            coin2rigidbody.AddTorque(dirX, dirY, dirZ);
-            coin3rigidbody.AddTorque(dirX, dirY, dirZ);
 
+            coin1Rigidbody.AddForce(transform.up * 750);
+            coin2Rigidbody.AddForce(transform.up * 600);
+            coin3Rigidbody.AddForce(transform.up * 700);
+
+            coin1Rigidbody.AddTorque(dirX1, dirY1, dirZ1);
+            coin2Rigidbody.AddTorque(dirX2, dirY2, dirZ2);
+            coin3Rigidbody.AddTorque(dirX3, dirY3, dirZ3);
         }
+
     }
 
     void Update()
     {
-        coin1Velocity = coin1rigidbody.velocity;
-        coin2Velocity = coin2rigidbody.velocity;
-        coin3Velocity = coin3rigidbody.velocity;
+        coin1Velocity = coin1Rigidbody.velocity;
+        coin2Velocity = coin2Rigidbody.velocity;
+        coin3Velocity = coin3Rigidbody.velocity;
     }
 }
